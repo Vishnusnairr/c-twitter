f39dfa5f5df980f1b6ff71d2c70e3c9d91de378c
Vishnu S Nair <vishnusnair225@gmail.com>
2025-05-03 14:41:26 +0530

    Add files via upload

---
+++ next.config.mjs
@@ -0,0 +1,8 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    domains: ["lh3.googleusercontent.com"], //allow image domain here other if needed
+  },
+};
+
+export default nextConfig;
